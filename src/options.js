6ab99c4e190997c3bae30bf2032fded9f0d5d7b3
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2026-02-27 13:58:27 -0300

    fix!: use `compiler.options.mode` to set the default value of `failOnError`

--- src/options.js
+++ src/options.js
@@ -3,7 +3,7 @@ const { validate } = require("schema-utils");
 const schema = require("./options.json");
 
 /** @typedef {import("eslint").ESLint.Options} ESLintOptions */
-/** @typedef {import('eslint').ESLint.LintResult} LintResult */
+/** @typedef {import("eslint").ESLint.LintResult} LintResult */
 
 /**
  * @callback FormatterFunction
@@ -52,7 +52,6 @@ function getOptions(pluginOptions) {
     extensions: "js",
     emitError: true,
     emitWarning: true,
-    failOnError: true,
     resourceQueryExclude: [],
     ...pluginOptions,
     ...(pluginOptions.quiet ? { emitError: true, emitWarning: false } : {}),
